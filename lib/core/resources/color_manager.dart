c603793c20c70f8d9ee37c0571e14056d4e9aeb2
Mariem Tayel <mariemtayel2@gmail.com>
2026-03-07 21:01:11 +0200

    native_splash

---
+++ lib/core/resources/color_manager.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+ abstract class ColorManager {
+  static Color primaryYello = const Color(0xffF6BD00);
+  static Color prinamrydarkGrey = const Color(0xff282A28);
+  static Color prinamrygrey = const Color(0xff57AA53);
+
+  static Color prinamryblack = const Color(0xff000000);
+  static Color prinamrywhite = const Color(0xffFFFFFF);
+  static Color prinamryRed = const Color(0xffe61f34);
+}
